301db8cdb688f187d686271213fe70c313be649b
choudel <chouaibh@gmail.com>
2022-07-07 13:17:21 +0100

    online store

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './controllers/app.controller';
-import { AppService } from './app.service';
+import { Module, Global } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsService } from './models/products.service';
+import { UsersService } from './models/users.service';
+import { Product } from './models/product.entity';
+import { User } from './models/user.entity';
+import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
 
+@Global()
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot({
+    "type": "mysql",
+    "host": "localhost",
+    "port": 3306,
+    "username": "root",
+    "password": "",
+    "database": "online_store",
+    "entities": ["dist/**/*.entity{.ts,.js}"],
+    "synchronize": true
+    }),
+    TypeOrmModule.forFeature([Product,User]),
+    AdminModule,
+    AuthModule,
+  ],
+  controllers: [AppController, ProductsController],
+  providers: [ProductsService,UsersService],
+  exports: [ProductsService, UsersService],
+
 })
 export class AppModule {}
---
+++ src/models/products.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './product.entity';
+@Injectable()
+export class ProductsService {
+constructor(
+@InjectRepository(Product)
+private productsRepository: Repository<Product>,
+) {}
+findAll(): Promise<Product[]> {
+return this.productsRepository.find();
+}
+findOneBy(id: number): Promise<Product> {
+return this.productsRepository.findOneBy({
+    id: id
+});
+}
+createOrUpdate(product: Product): Promise<Product> {
+    return this.productsRepository.save(product);
+    }
+async remove(id: number): Promise<void> {
+        await this.productsRepository.delete(id);
+        }
+            
+}
